d44de5b943b13ad785d8a668df4ed4c1605e2440
Øyvind Volden <oyvinvo@gmail.com>
2016-04-18 20:38:05 +0200

    A form example with callback on submit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import Title from './Title.jsx';
 import './home.css';
 import TitlePropTextChild from '../property-example/TitlePropTextChild.jsx';
+import ArrayToList from '../property-example/ArrayToList.jsx';
+
+var data = [{id: 1, navn: "Pete Hunt", text: "This is one comment"}, {id: 2, navn: "Jordan Walke", text: "This is *another* comment"}];
 
 export default React.createClass({
-   render() {
+
+
+    render() {
        return (
            <span>
                <Title />
                <div className="bold-text">Insert bold statement here.</div>
                <TitlePropTextChild title="Something interesting happened">Read more about it somewhere else</TitlePropTextChild>
                <TitlePropTextChild title="You won't believe what happened next">And you won't find out</TitlePropTextChild>
+               <ArrayToList/>
            </span>
        )
    }
---
+++ src/property-example/ArrayToList.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Form from './Form.jsx';
+
+export default React.createClass({
+    handleCommentSubmit: function(comment) {
+        var comments = this.state.data;
+        // not very robust
+        comment.id = Date.now();
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+        // TODO, post and read response into state after the opimistic update
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount() {
+      this.setState({data: [{"id": "1", "name": "per", "text":"Hiya"}, {"id": "2", "name": "Luke", "text":"yo"}]});
+    },
+    render: function() {
+        var commentNodes = this.state.data.map(function(comment) {
+            return (
+                <tr key={comment.id}>
+                    <td>{comment.name}</td>
+                    <td>{comment.id}</td>
+                    <td>{comment.text}</td>
+                </tr>
+            );
+        });
+        return (
+            <div>
+                <table className="commentList">
+                    <thead>
+                        <tr>
+                            <th>Navn</th>
+                            <th>id</th>
+                            <th>tekst</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {commentNodes}
+                    </tbody>
+                </table>
+                <Form onCommentSubmit={this.handleCommentSubmit} />
+            </div>
+        );
+    }
+});
